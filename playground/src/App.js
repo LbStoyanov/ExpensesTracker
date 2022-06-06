9ee3956c287b213557801755492921d7baaf9b93
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-06 22:52:45 +0300

    Done for today!

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,5 +1,35 @@
 import Expenses from "./Components/Expenses";
 
 function App(){
-  
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Krastavici',
+      amount: 150.13,
+      date: new Date(2022, 6, 6),
+    },
+    {id: 'e2', title: 'New TV',amount: 1432.49,date: new Date(2022, 6, 6)},
+    {
+      id: 'e3',
+      title: 'Koto IMA',
+      amount: 1550.13,
+      date: new Date(2022, 6, 6),
+    },
+    {
+      id: 'e4',
+      title: 'SIRISHTE',
+      amount: 150.13,
+      date: new Date(2022, 6, 6),
+    },
+  ];
+
+  return(
+    <div>
+      <h2>Aide Bace Pepi s nojleto</h2>
+      <Expenses items={expenses} />
+    </div>
+  );
+    
 }
+
+export default App;
