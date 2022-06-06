054b18b3a7209c9e5423ab438abd41efce121045
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-06 22:36:01 +0300

    ComponentsLessonFinished

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,41 +1,5 @@
-import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from "./Components/Expenses";
 
-function App() {
- 
-
-  const expense = [
-    {title: "Kaima", amount: 304.16, date: new Date(2022, 6, 2) },
-    { title: "Koto ima", amount: 304.16, date: new Date(2022, 6, 2) },
-    { title: "Krastavici", amount: 454.16, date: new Date(2022, 6, 2) },
-    { title: "Sirenie", amount: 954.16, date: new Date(2022, 6, 2) },
-  ];
-
-  return (
-    <div>
-      <h2>Let's get started,Badjo!</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      ></ExpenseItem>
-      
-    </div>
-  );
+function App(){
+  
 }
-
-export default App;
