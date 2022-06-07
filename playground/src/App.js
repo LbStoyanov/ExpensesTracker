6258017f39c748c6e97ec887676cada07ffba683
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-07 22:06:44 +0300

    Done!

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,12 +1,12 @@
-import Expenses from "./Components/Expenses";
+import Expenses from "./components/Expenses";
 
-function App(){
+function App() {
   const expenses = [
     {
       id: 'e1',
       title: 'Krastavici',
       amount: 150.13,
-      date: new Date(2022, 6, 6),
+      date:  new Date(2022, 5, 6) ,
     },
     {id: 'e2', title: 'New TV',amount: 1432.49,date: new Date(2022, 6, 6)},
     {
