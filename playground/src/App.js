3bdfe5b2584248508019e83108da12a037e56a32
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-25 12:39:19 +0300

    ReactJS

--- playground/src/App.js
+++ playground/src/App.js
@@ -8,20 +8,20 @@ function App() {
   const expenses = [
     {
       id: 'e1',
-      title: 'Krastavici',
+      title: 'Cars',
       amount: 150.13,
       date:  new Date(2022, 5, 6) ,
     },
     {id: 'e2', title: 'New TV',amount: 1432.49,date: new Date(2022, 6, 6)},
     {
       id: 'e3',
-      title: 'Koto IMA,Towa',
+      title: 'PS5 Games',
       amount: 1550.13,
       date: new Date(2022, 6, 6),
     },
     {
       id: 'e4',
-      title: 'BONIBONI',
+      title: 'Computer parts',
       amount: 150.13,
       date: new Date(2022, 6, 6),
     },
