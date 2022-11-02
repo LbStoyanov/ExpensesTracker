e85b92396f0e6ab6c9878adcd4b273ad72a9b0c1
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-02 18:44:50 +0200

    ReactJsStates

--- playground/src/components/Expenses/ExpenseItem.js
+++ playground/src/components/Expenses/ExpenseItem.js
@@ -4,15 +4,15 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  //function clickHandler(){}
+  
+  //Always returns array with two values!!!
   const [title,setTitle] = useState(props.title);
 
-  //let title = props.title;
-  const clicklHandler = () =>{
-    //title = 'Updated!'
+  
+  function clickHandler (){
     setTitle('Updated!');
-    console.log(title);
-  }
+  };
+  
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
@@ -20,7 +20,7 @@ function ExpenseItem(props) {
         <h2>{title}</h2>
         <div className='expense-item__price'>EUR:{props.amount}</div>
       </div>
-      <button onClick={clicklHandler}>Change title</button>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
